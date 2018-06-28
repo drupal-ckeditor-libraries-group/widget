3f355317e6fef1992eaa67eec146e308e62f3f6c
Wiktor Walc <w.walc@cksource.com>
2018-06-28 09:55:17 +0200

    Updated language files.

--- lang/et.js
+++ lang/et.js
@@ -2,7 +2,7 @@
  * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-CKEDITOR.plugins.setLang( 'widget', 'fa', {
-	'move': 'کلیک و کشیدن برای جابجایی',
-	'label': 'ابزارک %1'
+CKEDITOR.plugins.setLang( 'widget', 'et', {
+	'move': 'Click and drag to move', // MISSING
+	'label': '%1 vidin'
 } );
